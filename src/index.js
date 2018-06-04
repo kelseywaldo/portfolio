605951c87b601a955f803f8d4c12be98784d3d24
Kelsey McAlpine <kmcalpine023@gmail.com>
2018-06-03 18:09:16 -0700

    setup middleware

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from "redux";
+import promise from "redux-promise";
+import thunk from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import reducers from "./reducers";
+import App from './App';
+
+const createStoreWithMiddleware = applyMiddleware(promise, thunk)(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
