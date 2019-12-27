0cfadce92972f5b15349d9027e939f8d9baf84a6
Kelsey McAlpine <kmcalpine023@gmail.com>
2019-12-26 21:15:48 -0800

    use jsonp to fix cors error

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,17 +1,22 @@
 import axios from 'axios';
 import { GET_PROJECTS_LIST_SUCCESS } from './types';
+import jsonp from 'jsonp';
 
-const API_ROOT = 'https://cors.io/?https://www.behance.net/v2';
-const BEHANCE_CLIENT_ID = 'xxxxxxxxxxxxxxx';
+const API_ROOT = 'https://www.behance.net/v2';
+const BEHANCE_CLIENT_ID = 'RO19VGeyHz2nxjBUxjiO32Tr1FzKfqcb';
 
 export function getProjectsList(project_id) {
   return function(dispatch) {
-    return axios.get(`${API_ROOT}/users/kelseymcalpine/projects?client_id=${BEHANCE_CLIENT_ID}`).then(
-      response => {
-        dispatch({
-          type: GET_PROJECTS_LIST_SUCCESS,
-          projects: response.data.projects
-        });
+    return jsonp(`${API_ROOT}/users/kelseywaldo/projects?client_id=${BEHANCE_CLIENT_ID}`, null,
+      (err, data) => {
+        if (err) {
+          console.error(err.message);
+        } else {
+          dispatch({
+            type: GET_PROJECTS_LIST_SUCCESS,
+            projects: data.projects
+          });
+        }
       }
     )
   }
